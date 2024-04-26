a9cf51502b58374b2cf0580df3306985024b5df0
Carny2575 <milespilchik@gmail.com>
2024-04-26 10:38:20 -0400

    Switched binarySearch to linearSearch

--- Searcher.java
+++ Searcher.java
@@ -1,5 +1,5 @@
 public class Searcher {
-    public int binarySearch(int searchTerm, int[] searchSpace){
+    public int linearSearch(int searchTerm, int[] searchSpace){
         for(int i=0;i<searchSpace.length;i++){
             if(searchSpace[i] == searchTerm){
                 return i;
