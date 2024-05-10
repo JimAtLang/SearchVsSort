5e6f29e456a6a9e2f50a7d41f8588b7c366796e4
jimatlang <jim.stewart@thelangschool.org>
2024-05-10 10:15:46 -0400

    added binary search

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 public class Main {
     private Random r;
@@ -8,17 +9,31 @@ public class Main {
     }
     public Main(){
         r = new Random();
-        dataSize = 1000;
+        dataSize = 100000000;
+        int datamax = 10000000;
         int[] data = new int[dataSize];
         for(int i=0;i<dataSize;i++){
             data[i] = r.nextInt(1000000);
         }
 
-        Sorter srt = new Sorter();
-        srt.selectionSort(data);
-        for(int n:data){
-            System.out.print(n + " ");
-        }
+        // Sorter srt = new Sorter();
+        // srt.selectionSort(data);
+        Arrays.sort(data);
+        System.out.println("data sorted");
+        // for(int n:data){
+        //     System.out.print(n + " ");
+        // }
         System.out.println(" ");
+        Searcher sch = new Searcher();
+        int searchTerm = r.nextInt(datamax);
+        if(r.nextBoolean()){
+            int stPos = r.nextInt(dataSize);
+            searchTerm = data[stPos];
+            System.out.println("searchTerm is " + searchTerm + " at position " + stPos + " in data");
+        } else {
+            System.out.println("searchTerm is random number  " + searchTerm);
+        }
+        System.out.println("searchTerm = " + searchTerm);
+        System.out.println(sch.binarySearch(searchTerm, data));
     }
 }
--- Searcher.java
+++ Searcher.java
@@ -1,10 +1,30 @@
 public class Searcher {
-    public int binarySearch(int searchTerm, int[] searchSpace){
+    public int linearSearch(int searchTerm, int[] searchSpace){
         for(int i=0;i<searchSpace.length;i++){
             if(searchSpace[i] == searchTerm){
                 return i;
             }
         }
+        return -1;
+    }
+    public int binarySearch(int searchTerm, int[] searchSpace){
+        int steps = 0;
+        int min = 0;
+        int max = searchSpace.length-1;
+        while(min+1<max){
+            int current = (min+max)/2;
+            steps++;
+            if(searchSpace[current] == searchTerm){
+                System.out.println("took " + steps + " steps");
+                return current;
+            }
+            else if(searchSpace[current]>searchTerm){
+                max = current;
+            } else {
+                min = current;
+            }
+        }
+        System.out.println("took " + steps + " steps");
         return -1;
     }
 }
